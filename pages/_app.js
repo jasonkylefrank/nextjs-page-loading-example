f4762df04efb96f0a1cd08a1be10308ced3c5cb2
Jason K. Frank <jasonkylefrank@gmail.com>
2018-08-19 08:20:17 -0400

    Initial commit.

---
+++ pages/_app.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import App, {Container} from 'next/app'
+import Link from 'next/link'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+const linkStyle = {
+  margin: '0 10px 0 0'
+}
+
+console.log(Router);
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+// --- Code from before the "fix" 5 days ago 
+//     (the other way to  solve the crash is to update the version of "next" in the package.json)
+// Router.onRouteChangeStart = (url) => {
+//   console.log(`Loading: ${url}`)
+//   NProgress.start()
+// }
+// Router.onRouteChangeComplete = () => NProgress.done()
+// Router.onRouteChangeError = () => NProgress.done()
+
+
+export default class MyApp extends App {
+  static async getInitialProps ({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return {pageProps}
+  }
+
+  render () {
+    const {Component, pageProps} = this.props
+    return (
+      <Container>
+        <div style={{ marginBottom: 20 }}>
+          <Link href='/'><a style={linkStyle}>Home</a></Link>
+          <Link href='/about'><a style={linkStyle}>About</a></Link>
+          <Link href='/forever'><a style={linkStyle}>Forever</a></Link>
+          <Link href='/non-existing'><a style={linkStyle}>Non Existing Page</a></Link>
+        </div>
+
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
